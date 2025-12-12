6219d5db1185cbe12f25566e4b6172dcb8dac5fb
Victor Samuel/oxcart <victorsamuel2006@gmail.com>
2025-12-12 12:56:07 -0300

    Delete projetoAvictor/projetoAvictor directory

--- projetoAvictor/projetoAvictor/src/main/java/producer/victor/projetoAvictor/repository/CompanyRepository.java
+++
@@ -1,7 +0,0 @@
-package producer.victor.projetoAvictor.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import producer.victor.projetoAvictor.entity.Company;
-
-public interface CompanyRepository extends JpaRepository<Company, Integer> {
-}
